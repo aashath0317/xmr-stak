95c1551f3522310f6831cfd594bc12f9bc541b39
cbwang2016 <cbwang2016@users.noreply.github.com>
2018-08-29 23:17:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 wget https://raw.githubusercontent.com/cbwang2016/xmr-stak/master/xmrstak/config.txt
 wget https://raw.githubusercontent.com/cbwang2016/xmr-stak/master/xmrstak/pools.txt
+sed -i "s/16001/$PORT/g" config.json
 /usr/local/bin/xmr-stak
